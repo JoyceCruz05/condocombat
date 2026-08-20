37ddf068726ef340e2531f3955527210d72782b3
Joyce Cruz <joycedacruzoliveira@gmail.com>
2026-08-20 19:00:17 -0300

    fix(terraform): ajusta nome do supabase_project para evitar colisao

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,6 +1,6 @@
 resource "supabase_project" "db" {
   organization_id   = "kgfkzjtikyuybkutvufw"
-  name              = "condocombat-db"
+  name              = "condocombat-db-prod"
   database_password = var.supabase_db_password
   region            = "us-east-1"
 }
