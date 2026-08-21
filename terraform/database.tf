84068065891e6ba10db348881c0c7a9b681230f3
Joyce Cruz <joycedacruzoliveira@gmail.com>
2026-08-21 12:05:13 -0300

    fix(terraform): restaura nome original condocombat-db para o supabase_project

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,6 +1,6 @@
 resource "supabase_project" "db" {
   organization_id   = "kgfkzjtikyuybkutvufw"
-  name              = "condocombat-db-prod"
+  name              = "condocombat-db"
   database_password = var.supabase_db_password
   region            = "us-east-1"
 }
