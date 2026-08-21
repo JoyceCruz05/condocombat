eb70a170693b9d1554235ea50bce4c98b287cf18
Joyce Cruz <joycedacruzoliveira@gmail.com>
2026-08-21 12:36:39 -0300

    fix(netlify): altera chave da variavel para PUBLIC_APP_URL conforme desafio 3

--- terraform/landing.tf
+++ terraform/landing.tf
@@ -1,14 +1,12 @@
 # Busca a referência do site da Landing Page na Netlify
 data "netlify_site" "landing" {
-  name      = var.netlify_site_name
-  team_slug = "joycecruz05"
+  name = var.netlify_site_name
 }
 
 # Configura a variável de ambiente PUBLIC_APP_URL com a URL gerada para o Frontend (Render)
 resource "netlify_environment_variable" "landing_public_url" {
-  site_id   = data.netlify_site.landing.id
-  key       = "NEXT_PUBLIC_FRONTEND_URL"
-
+  site_id = data.netlify_site.landing.id
+  key     = "PUBLIC_APP_URL"
   values = [
     {
       value   = render_web_service.frontend.url
@@ -16,6 +14,7 @@ resource "netlify_environment_variable" "landing_public_url" {
     }
   ]
 }
+
 # Fazer o deploy dos arquivos compilados (landing/dist) gerados no CI via Terraform
 resource "terraform_data" "landing_deploy" {
   triggers_replace = [
@@ -24,8 +23,7 @@ resource "terraform_data" "landing_deploy" {
   ]
 
   provisioner "local-exec" {
-    interpreter = ["powershell", "-Command"]
-    command     = "Compress-Archive -Path ..\\landing\\dist\\* -DestinationPath ..\\site.zip -Force; curl.exe -s -X POST -H 'Content-Type: application/zip' -H 'Authorization: Bearer ${var.netlify_api_token}' --data-binary '@..\\site.zip' https://api.netlify.com/api/v1/sites/${data.netlify_site.landing.id}/deploys"
+    command = "npx netlify-cli deploy --dir=${path.module}/../landing/dist --prod --auth=${var.netlify_api_token} --site=${data.netlify_site.landing.id}"
   }
 
   depends_on = [
